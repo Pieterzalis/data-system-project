9e26d13dd53467dfea62eb5ab6bd979b81f70581
Florijn Burggraaf <florijnburggraaf@gmail.com>
2019-01-14 13:25:25 +0100

    read_qf returns dictionary now

--- Backend/read_qf.py
+++ Backend/read_qf.py
@@ -1,6 +1,7 @@
 import PyPDF2
 import sys
 import re
+import os
 from nltk.corpus import stopwords
 import json
 from gensim.corpora.dictionary import Dictionary
@@ -19,12 +20,13 @@ def read_doc(target):
 	return full_text
 	
 def doc_to_metadata(doc):
+	metadict = {} #dictionary with metadata
 	doc = doc.replace('Vragen door','Vragen van').replace('\naan', ' aan ').replace('aan\n',' aan ').replace('inzake','over').replace('\n','')
-	indiener = doc.split('Vragen van')[1].split('aan')[0].strip()
-	topic = doc.split('over')[1].split('(')[0].strip()
-	id = doc.split('der Kamer')[1].split('Vragen')[0].strip()
-	date = doc.split('ingezonden')[1].split(')')[0].strip()
-	return [id, topic, indiener, date]
+	metadict['indiener'] = doc.split('Vragen van')[1].split('aan')[0].strip()
+	metadict['topic'] = doc.split('over')[1].split('(')[0].strip()
+	metadict['id'] = doc.split('der Kamer')[1].split('Vragen')[0].strip()
+	metadict['date'] = doc.split('ingezonden')[1].split(')')[0].strip()
+	return metadict
 
 #Parse questions from file text
 def doc_to_questions(doc):
@@ -32,10 +34,11 @@ def doc_to_questions(doc):
 	questions = questions[1:]
 	for i in range(len(questions)):
 		question = questions[i]
-		question = question.lower()
 		question = question[:question.rfind('?')]
 		question = re.sub(r'-\n','',question) # handle dashes breaking up words at end of line
 		question = re.sub(r'\n',' ',question)
+		question = 'Vraag ' + question + '?'
+		question = re.sub(r'\s+', ' ', question)
 		#To do: Handle footnotes ending up in questions crossing the page boundary. e.g. 2018D50780_ 2018-10-24.
 		#Proposed solution: first and last of the words to remove seem always to contain both letters and number, use that to remove them.
 		#To do: special characters (e met puntjes)
@@ -62,7 +65,8 @@ def questions_to_keywords(questions, per_question):
 		if per_question:
 			keywords_per_question = []
 			for question in questions:
-				question = re.sub("'",' ',question).replace('_', ' ')
+				question = question.lower()
+				question = re.sub("'",' ',question).replace('_', ' ').replace(' -',' ')
 				question = re.sub(r'[^A-Za-z^-]', ' ', question)
 				question = re.sub(r'\s+', ' ', question)
 				words = [word for word in question.split() if word not in stopwords.words('dutch')]
@@ -71,35 +75,29 @@ def questions_to_keywords(questions, per_question):
 		else:
 			for i in range(len(questions)):
 				question = questions[i]
-				question = re.sub("'",' ',question).replace('_', ' ')
+				question = question.lower()
+				question = re.sub("'",' ',question).replace('_', ' ').replace(' -',' ')
 				question = re.sub(r'[^A-Za-z^-]', ' ', question)
 				question = re.sub(r'\s+', ' ', question)
 				words = [word for word in question.split() if word not in stopwords.words('dutch')]
 				questions[i] = words
 			questions = [word for question in questions for word in question]
 			return tf_idf_keywords(questions,bow,dictionary)
-
-def construct_returnvalue(metadata,questions,keywords):
-	print(metadata)
-	print(questions)
-	print(keywords)
-	returnvalue = ''
-	for section in [metadata, questions, keywords]:
-		for el in section:
-			returnvalue += el + 'breakbreak'
-		returnvalue += 'sectionsplit'
-	return returnvalue
-			
+		
 def main(argv):
-	target = argv[1]
+	target = argv[1].replace('%26','&').replace('%20',' ')
+	print(target)
 	doc = read_doc(target)
 	metadata = doc_to_metadata(doc)
 	questions = doc_to_questions(doc)
-	questions_copy = questions.copy()
+	questions_orig = questions.copy()
 	keywords = questions_to_keywords(questions, False)
-	returnstring = construct_returnvalue(metadata,questions_copy,keywords)
-	print(returnstring)
-	return returnstring
+	returndict = {}
+	returndict['metadata'] = metadata
+	returndict['questions'] = questions_orig
+	returndict['keywords'] = keywords
+	print(returndict)
+	return
 	
 if __name__ == "__main__":
 	main(sys.argv)
