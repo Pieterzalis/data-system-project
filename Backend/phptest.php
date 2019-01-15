71a33bf68522399a6cf9e107b2a6e15b2db24c90
Christian Swier <christian.swier@hotmail.com>
2019-01-15 16:50:36 +0100

    PHPtest will now for a big part prepare json data for db insertion, not finished yet.

--- Backend/phptest.php
+++ Backend/phptest.php
@@ -1,35 +1,122 @@
 <?php
 #$command = escapeshellcmd("python C:\\Users\\flori\\OneDrive\\Documenten\\hello.py");
 #$output = shell_exec($command);
+require_once '../website/database/Model_Core.php';
 
+parseLetter();
+
+function parseLetter()
+{
     // TODO make this dynamic with the upload script!
-    $file_path = htmlentities('read_qf.py C:\\xampp\htdocs\\datasystems.test\\Backend\\letter2.pdf');
+    $file_path = htmlentities('C:\\xampp\htdocs\\datasystems.test\\Backend\\letter2.pdf');
+    $python_path = htmlentities('read_qf.py');
     $file_name = basename($file_path);
 
-    exec("python " . $file_path . "", $output, $ret_code);
-    //echo end($output);
+    // Execute python script for parsing the letter
+    exec("python " . $python_path . " " . $file_path . "", $output, $ret_code);
 
     $output_data = end($output);
 
-    if ($output_data===false) {
+    if ($output_data === false) {
         $output_data = null;
-        echo 'Python returned no output';
-    }
-    else {
+        echo 'Error: Python returned no output';
+    } else {
         $json = json_decode($output_data);
-        //echo $json;
 
-// Now decode indiener to only have names and party
-        $test = 1;
-        echo '<h2>Parse results of letter: '. $file_name .'</h2>';
+        echo '<h2>Raw parse results of letter: ' . $file_name . '</h2>';
         echo '<p>Project Code: ' . $json->metadata->id . '</p>';
         echo '<p>Indiener: ' . $json->metadata->indiener . '</p>';
         echo '<p>Topic of projet: ' . $json->metadata->topic . '</p>';
         echo '<p>Date of letter: ' . $json->metadata->date . '</p>';
 
+        echo '<h3>Questions in letter:</h3>';
+        foreach ($json->questions as $question) {
+            echo '<p>' . $question . '</p>';
+        }
+
+        echo '<h3>Keywords extracted from letter:</h3>';
+        foreach ($json->keywords as $keyword) {
+            echo '<p>' . $keyword . '</p>';
+        }
+
+        prepareDataforDB($json);
+
+    }
+}
+
+function prepareDataforDB($data)
+{
+    // Transform JSON object elements to workable strings to save to DB
+    echo '<h1> Preparing data for db for Project Code: ' . $data->metadata->id . '</h1>';
+
+    $id = $data->metadata->id;
+    $indiener = getIndiener($data->metadata->indiener);
+    $party = getParty($data->metadata->indiener);
+    $title = ucfirst($data->metadata->topic);
+    $date = dutch_strtotime($data->metadata->date);
+
+    $questions = getQuestions($data->questions);
+
+    $keywords = $data->keywords;
+
+    // When done, call save to database function.
+
+    // Echo results
+    echo '<p>Project Code: ' . $id . '</p>';
+    echo '<p>Indiener: ' . $indiener . '</p>';
+    echo '<p>Topic of projet: ' . $title . '</p>';
+    echo '<p>Date of letter: ' . $date . '</p>';
+
+    echo '<h3>Questions in letter:</h3>';
+    foreach ($questions as $question) {
+        echo '<p>' . $question . '</p>';
     }
 
+    echo '<h3>Keywords extracted from letter:</h3>';
+    foreach ($keywords as $keyword) {
+        echo '<p>' . $keyword . '</p>';
+    }
 
+}
+
+function getIndiener($string)
+{
+    // TODO add support for strings with 'leden'
+    // Need to set substring so it only contains indiener name
+    $str = substr($string, 4, (strpos($string, '(')-1)-4);
+
+    return $str;
+}
+
+function getParty($string)
+{
+    // TODO fix this
+    // Need to set substring so it only contains indiener name
+    $strpos_start = strpos($string, '(') +1;
+    $strpos_end = strpos($string, ')' ) ;
+
+    $str = substr($string, $strpos_start);
+
+    return $str;
+}
+
+function getQuestions($questions){
+
+    // Strip 'vraag 1; vraag 2; etc from question
+    $questions_stripped = array();
+
+    $i = 1;
+    foreach ($questions as $question) {
+
+        // Get position of question number
+        $strpos_number = strpos($question, ''.$i.'') + 2; // Add 2 to include the number and space after it
+        array_push($questions_stripped, (substr($question, $strpos_number)));
+
+        $i++;
+    }
+
+    return $questions_stripped;
+}
 
 
 
