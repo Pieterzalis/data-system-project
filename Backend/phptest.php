f62c4b911c5e70587d69f8d0efda3c8c8aefe67f
Christian Swier <christian.swier@hotmail.com>
2019-01-15 15:04:50 +0100

    phptest.php will now properly function when executing read_qf.py and returning an JSON object.

--- Backend/phptest.php
+++ Backend/phptest.php
@@ -2,24 +2,34 @@
 #$command = escapeshellcmd("python C:\\Users\\flori\\OneDrive\\Documenten\\hello.py");
 #$output = shell_exec($command);
 
-// TODO make this dynamic with the upload script!
-    $file_path = htmlentities('C:\\Users\\Christian\\Desktop\\xampp\\htdocs\\datasystems.test\\Backend\\read_qf.py C:\\Users\\Christian\\Desktop\\xampp\\htdocs\\datasystems.test\\Backend\\letter.pdf');
+    // TODO make this dynamic with the upload script!
+    $file_path = htmlentities('read_qf.py C:\\xampp\htdocs\\datasystems.test\\Backend\\letter2.pdf');
+    $file_name = basename($file_path);
 
     exec("python " . $file_path . "", $output, $ret_code);
-    echo end($output);
+    //echo end($output);
 
-    if (!isset($output[6])) {
-        $output[6] = null;
+    $output_data = end($output);
+
+    if ($output_data===false) {
+        $output_data = null;
+        echo 'Python returned no output';
     }
-
-    $test = 1;
-    $json = json_decode($output[6]);
+    else {
+        $json = json_decode($output_data);
+        //echo $json;
 
 // Now decode indiener to only have names and party
-    $test = 1;
+        $test = 1;
+        echo '<h2>Parse results of letter: '. $file_name .'</h2>';
+        echo '<p>Project Code: ' . $json->metadata->id . '</p>';
+        echo '<p>Indiener: ' . $json->metadata->indiener . '</p>';
+        echo '<p>Topic of projet: ' . $json->metadata->topic . '</p>';
+        echo '<p>Date of letter: ' . $json->metadata->date . '</p>';
 
-    echo $json->metadata->id;
-#echo $ret_code;
+    }
+
+
 
 
 
--- Backend/read_qf.py
+++ Backend/read_qf.py
@@ -1,6 +1,3 @@
-#!C:\Users\Christian\AppData\Local\Programs\Python\Python37-32\python.exe
-print ("Content-Type: text/plain;charset=utf-8\n)
-
 import PyPDF2
 import sys
 import re
@@ -10,7 +7,7 @@ import json
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.tfidfmodel import TfidfModel
 
-#Take file location and return text of file
+# Take file location and return text of file
 
 
 def read_doc(target):
@@ -26,16 +23,16 @@ def read_doc(target):
 
 
 def doc_to_metadata(doc):
-	metadict = {} #dictionary with metadata
-	doc = doc.replace('Vragen door','Vragen van').replace('\naan', ' aan ').replace('aan\n',' aan ').replace('inzake','over').replace('\n','')
+	metadict = {} # dictionary with metadata
+	doc = doc.replace('Vragen door', 'Vragen van').replace('\naan', ' aan ').replace('aan\n', ' aan ').replace('inzake', 'over').replace('\n', '')
 	metadict['indiener'] = doc.split('Vragen van')[1].split('aan')[0].strip()
 	metadict['topic'] = doc.split('over')[1].split('(')[0].strip()
 	metadict['id'] = doc.split('der Kamer')[1].split('Vragen')[0].strip()
-	metadict['date'] = doc.split('ingezonden')[1].split(')')[0].strip()
+	metadict['date'] = doc.split('Ingezonden')[1].split(')')[0].strip()
 	return metadict
 
 
-#Parse questions from file text
+# Parse questions from file text
 def doc_to_questions(doc):
 	questions = doc.split('Vraag ')
 	questions = questions[1:]
@@ -47,27 +44,28 @@ def doc_to_questions(doc):
 		question = 'Vraag ' + question + '?'
 		question = re.sub(r'\s+', ' ', question)
 
-
-		#To do: Handle footnotes ending up in questions crossing the page boundary. e.g. 2018D50780_ 2018-10-24.
-		#Proposed solution: first and last of the words to remove seem always to contain both letters and number, use that to remove them.
-		#To do: special characters (e met puntjes)
+		# To do: Handle footnotes ending up in questions crossing the page boundary. e.g. 2018D50780_ 2018-10-24.
+		# Proposed solution: first and last of the words to remove seem always to contain both letters and number,
+		# use that to remove them.
+		# To do: special characters (e met puntjes)
 		questions[i] = question
 	return questions
 
 
-#Get keywords out of text by tf_idf method
+# Get keywords out of text by tf_idf method
 def tf_idf_keywords(text, bow, dictionary):	
-	tfidf = TfidfModel(bow)#generates the model
+	tfidf = TfidfModel(bow) # generates the model
 	text = dictionary.doc2bow(text)
 	tfidf_weights = tfidf[text]  
-	sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True) #sort by value
+	sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True) # sort by value
 	keywords = []
 	for term_id, weight in sorted_tfidf_weights[:5]:
 		print(str(dictionary.get(term_id)), weight)
 		keywords.append(str(dictionary.get(term_id)))
 	return keywords
 
-#Given questions, get keywords. Can be done both per question or for the set of questions				
+
+# Given questions, get keywords. Can be done both per question or for the set of questions
 def questions_to_keywords(questions, per_question):
 	with open('corpus.json') as bowfile:
 		bow = json.load(bowfile)
@@ -93,9 +91,10 @@ def questions_to_keywords(questions, per_question):
 				questions[i] = words
 			questions = [word for question in questions for word in question]
 			return tf_idf_keywords(questions,bow,dictionary)
-		
+
+
 def main(argv):
-	target = argv[1].replace('%26','&').replace('%20',' ')
+	target = argv[1].replace('%26', '&').replace('%20', ' ')
 	print(target)
 	doc = read_doc(target)
 	metadata = doc_to_metadata(doc)
@@ -103,10 +102,10 @@ def main(argv):
 	questions_orig = questions.copy()
 	keywords = questions_to_keywords(questions, False)
 
-	#returndict = {}
-	#returndict['metadata'] = metadata
-	#returndict['questions'] = questions_orig
-	#returndict['keywords'] = keywords
+	# returndict = {}
+	# returndict['metadata'] = metadata
+	# returndict['questions'] = questions_orig
+	# returndict['keywords'] = keywords
 
 	# json is easier to decode in php
 	# please check this florijn!!
@@ -118,7 +117,8 @@ def main(argv):
 
 	print(json_data)
 	return
-	
+
+
 if __name__ == "__main__":
 	main(sys.argv)
 	
