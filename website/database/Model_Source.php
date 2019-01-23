fc9ec347110eb4e6b85f55fffea6b3733c359007
Christian Swier <christian.swier@hotmail.com>
2019-01-23 12:58:00 +0100

    Remove sources from Knowledge Base now works.

--- website/database/Model_Source.php
+++ website/database/Model_Source.php
@@ -5,6 +5,42 @@
 // This will also load DB config
 require_once 'Model_Core.php';
 
+//
+// Handle any ajax post requests to this file here
+//
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_REQUEST['func'])) {
+
+        if ($_REQUEST['func'] === 'removeFromKnowledgeBase' && $_REQUEST['id'] != '') {
+            removeFromKnowledgeBase($_REQUEST['id']);
+        }
+    }
+
+}
+
+// Set given source_id as inactive
+function removeFromKnowledgeBase($source_id_string) {
+    $i=1;
+
+    $str_explode = explode("-", $source_id_string);
+    $source_id = (int)end($str_explode);
+
+    $i=1;
+
+    $query = makeSourceInactive($source_id);
+
+    if ($query != 1) {
+        // Error removing source from the database..
+
+        echo 'Error removing source from question knowledge base';
+
+    } else {
+        echo 'success';
+    }
+
+
+}
 
 //Method to save source data to database
 function saveSourceArray($sources, $question_id) {
@@ -30,4 +66,14 @@ function saveSourceArray($sources, $question_id) {
 
 	// Return the source IDs
 	return $source_ids;
+}
+
+function makeSourceInactive($source_id){
+    DB::update('source', array(
+        'source_active' => 0), "source_id=%i", $source_id);
+
+    $counter = DB::affectedRows();
+    $i =1;
+
+    return $counter;
 }
