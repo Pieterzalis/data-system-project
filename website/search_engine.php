3f3ea398380ea96888ee83fcbd0a05396ceb9dab
Florijn Burggraaf <florijnburggraaf@gmail.com>
2019-01-24 14:22:06 +0100

    keyword disable

--- website/search_engine.php
+++ website/search_engine.php
@@ -38,9 +38,11 @@ $keywords = DB::query("select keyword_name from keyword where keyword_project_id
     <script src="bootstrap/js/bootstrap.min.js"></script>
 	<script>
         $(document).ready(function () {
-
+			$('.badge-secondary').click(function(){
+				$(this).toggleClass('active-keyword');
+				$(this).toggleClass('nonactive-keyword');
+			});
             $(".do_search").click(function () {
-
                     $("#sourcecontainer").html("Searching for relevant sources...");
                     var use_news_articles = $('#gridCheck1:checked').val();
                     if (use_news_articles == undefined){
@@ -62,7 +64,7 @@ $keywords = DB::query("select keyword_name from keyword where keyword_project_id
                     console.log(use_old_answers);
                     var userkeywords = $("#Searchfield").val().split(',');
                     var autokeywords = [];
-                    $(".keywordtags").children('span').each(function(index){
+                    $(".keywordtags").children('.active-keyword').each(function(index){
                         autokeywords.push($(this).text());
                     });
                     keywords = userkeywords.concat(autokeywords);
@@ -162,7 +164,7 @@ $keywords = DB::query("select keyword_name from keyword where keyword_project_id
                                                 <div class="keywordtags"><?php
 								                //#列出该project的所有keyword
 								                foreach($keywords as $v){
-                                                    echo '<span class="badge badge-pill badge-secondary">'.$v['keyword_name'].'</span>';
+                                                    echo '<span class="badge badge-pill badge-secondary active-keyword">'.$v['keyword_name'].'</span>';
 								                }
 								                	?>
                                                 </div>
